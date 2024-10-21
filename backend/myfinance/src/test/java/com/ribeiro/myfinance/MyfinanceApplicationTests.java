eec433a08ee3d45e199b15ad47d82593e9e6cec0
Thiago Ribeiro <thiago.ribeiroc.0810@gmail.com>
2024-10-21 15:34:10 -0300

    Criação do backend utilizando Java Spring Boot

---
+++ backend/myfinance/src/test/java/com/ribeiro/myfinance/MyfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ribeiro.myfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
